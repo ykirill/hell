b22a8cd64dab2f82fdf64caec46c1c2a5c0a90f5
nemaeska <k.n.yakushev@gmail.com>
2019-01-23 22:39:05 +0300

    Chapter 2. Edited DataTypes.hs file. Added two implementations of clientName (getter) function.

--- src/chapter2/src/Chapter2/DataTypes.hs
+++ src/chapter2/src/Chapter2/DataTypes.hs
@@ -1,5 +1,8 @@
+{-# LANGUAGE ViewPatterns, NamedFieldPuns, RecordWildCards #-}
+
 module Chapter2.DataTypes where
 
+{- Clients -}
 data Client = GovOrg     String
             | Company    String Integer Person String
             | Individual Person Bool
@@ -9,3 +12,22 @@ data Person = Person String String Gender
             deriving (Show)
 
 data Gender = Male | Female | Unknown deriving (Show)
+
+-- Long version
+clientName :: Client -> Maybe String
+clientName client = case client of
+           GovOrg name                                -> Just name
+           Company name _ _ _                         -> Just name
+           Individual (Person firstName lastName _) _ -> Just (firstName ++ " " ++ lastName)
+
+-- Short version
+clientName' :: Client -> Maybe String
+clientName' (GovOrg name)                                = Just name
+clientName' (Company name _ _ _)                         = Just name
+clientName' (Individual (Person firstName lastName _) _) = Just (firstName ++ " " ++ lastName)
+
+{- TimeMachines -}
+data TimeMachine = TimeMachine String Integer String Type Double
+                 deriving (Show)
+
+data Type = Future | Past | Both deriving (Show)
