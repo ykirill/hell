f348a3f11ec976c34c52062e64fbafbd13cd2fd8
nemaeska <k.n.yakushev@gmail.com>
2019-01-23 21:34:21 +0300

    Chapter 2. Edited SimpleFunctions.hs file. Added (+++) function.

--- src/chapter2/src/Chapter2/SimpleFunctions.hs
+++ src/chapter2/src/Chapter2/SimpleFunctions.hs
@@ -2,3 +2,8 @@ module Chapter2.SimpleFunctions where
 
 firstOrEmpty :: [ String ] -> String
 firstOrEmpty list = if not (null list) then head list else "empty"
+
+(+++) :: [a] -> [a] -> [a]
+list1 +++ list2 = if null list1
+                  then list2
+                  else (head list1) : (tail list1 +++ list2)
