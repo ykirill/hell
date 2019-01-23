30b6791ac30abd9bf6d8d3b7afac6ccc72dc7026
nemaeska <k.n.yakushev@gmail.com>
2019-01-23 22:03:00 +0300

    Chapter 2. Edited DataTypes.hs file. Added data type Person, improved data types Company and Individual.

--- src/chapter2/src/Chapter2/DataTypes.hs
+++ src/chapter2/src/Chapter2/DataTypes.hs
@@ -1,6 +1,9 @@
 module Chapter2.DataTypes where
 
 data Client = GovOrg     String
-            | Company    String Integer String String
-            | Individual String String Bool
+            | Company    String Integer Person String
+            | Individual Person Bool
+            deriving (Show)
+
+data Person = Person String String
             deriving (Show)
