7e571c762780d1687242b4d61e7104345b1da3b9
nemaeska <k.n.yakushev@gmail.com>
2019-01-26 12:18:04 +0300

    Chapter 2. Edited SimpleFunctions.hs file. Added function `sorted`, which determine that list is sorted.

--- src/chapter2/src/Chapter2/SimpleFunctions.hs
+++ src/chapter2/src/Chapter2/SimpleFunctions.hs
@@ -48,3 +48,8 @@ fibonacci :: Integer -> Integer
 fibonacci 0 = 0
 fibonacci 1 = 1
 fibonacci n = fibonacci (n-1) + fibonacci (n-2)
+
+sorted :: [Integer] -> Bool
+sorted []       = True
+sorted [_]      = True
+sorted (x:y:zs) = x < y && sorted (y:zs)
